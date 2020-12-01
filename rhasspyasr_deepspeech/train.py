a11dc68c6bdfde660f01fb0a3951ec377801981d
Sh4der <luk-bnr@gmx.de>
2020-12-01 17:59:22 +0100

    add training

--- rhasspyasr_deepspeech/train.py
+++ rhasspyasr_deepspeech/train.py
@@ -15,7 +15,7 @@ _LOGGER = logging.getLogger("rhasspyasr_deepspeech")
 def train(
     graph: nx.DiGraph,
     language_model: typing.Union[str, Path],
-    trie_path: typing.Union[str, Path],
+    scorer_path: typing.Union[str, Path],
     alphabet_path: typing.Union[str, Path],
     vocab_path: typing.Optional[typing.Union[str, Path]] = None,
     language_model_fst: typing.Optional[typing.Union[str, Path]] = None,
@@ -34,70 +34,97 @@ def train(
     ):
         base_fst_weight = (base_language_model_fst, base_language_model_weight)
 
-    # Begin training
-    with tempfile.NamedTemporaryFile(mode="w+") as arpa_file:
-        # 1. Create language model
-        _LOGGER.debug("Converting to ARPA language model")
-        rhasspynlu.arpa_lm.graph_to_arpa(
-            graph,
-            arpa_file.name,
-            model_path=language_model_fst,
-            base_fst_weight=base_fst_weight,
-            merge_path=mixed_language_model_fst,
-            vocab_path=vocab_path,
-        )
-
-        arpa_file.seek(0)
-
-        with tempfile.NamedTemporaryFile(mode="w+") as lm_file:
-            # 2. Convert to binary language model
-            arpa_to_binary(arpa_file.name, lm_file.name)
-
-            lm_file.seek(0)
-
-            with tempfile.NamedTemporaryFile(mode="w+") as trie_file:
-                # 3. Generate trie
-                make_trie(alphabet_path, lm_file.name, trie_file.name)
-
-                # Copy over actual files
-                lm_file.seek(0)
-                shutil.copy(lm_file.name, language_model)
-                _LOGGER.debug("Wrote binary language model to %s", language_model)
-
-                trie_file.seek(0)
-                shutil.copy(trie_file.name, trie_path)
-                _LOGGER.debug("Wrote trie to %s", trie_path)
+    with tempfile.TemporaryDirectory() as tmp_folder:
+        # Begin training
+        with tempfile.NamedTemporaryFile(mode="w+") as arpa_file:
+            # 1. Create language model
+            _LOGGER.debug("Converting to ARPA language model")
+            _LOGGER.debug("With: arpa: %s | modelpath: %s | base_fst_weight: %s | merge_path: %s | vocab_path: %s", arpa_file.name, language_model_fst, base_fst_weight, mixed_language_model_fst, vocab_path)
+            if(vocab_path == None):
+                vocab_path = str(tmp_folder) + "/vocab-500000.txt"
+            
+            rhasspynlu.arpa_lm.graph_to_arpa(
+                graph,
+                arpa_file.name,
+                model_path=language_model_fst,
+                base_fst_weight=base_fst_weight,
+                merge_path=mixed_language_model_fst,
+                vocab_path=vocab_path,
+            )
 
 
-def arpa_to_binary(
-    arpa_path: typing.Union[str, Path], binary_lm_path: typing.Union[str, Path]
+        _LOGGER.debug("Converting to vocab to binary language model")
+        _LOGGER.debug("With: arpa_file: %s | tmp_folder: %s ", arpa_file.name, tmp_folder)
+        vocab_to_binary(vocab_path, tmp_folder)
+
+
+        with tempfile.NamedTemporaryFile(mode="w+") as scorer_file:
+            # 3. Generate trie
+            make_scorer(vocab_path, scorer_file.name, alphabet_path, tmp_folder)
+
+            scorer_file.seek(0)
+            shutil.copy(scorer_file.name, scorer_path)
+            _LOGGER.debug("Wrote scorer to %s", scorer_path)
+
+
+def vocab_to_binary(
+    vocab_path: typing.Union[str, Path], lm_binary_dir: typing.Union[str, Path]
 ):
-    """Convert ARPA language model to binary format using kenlm."""
-    # NOTE: Using -i because other LM tools mistakenly produce positive log
-    # probabilities. This option sets those to 0.
+    """Convert vocab language model to binary format using kenlm."""
     binary_command = [
-        "build_binary",
-        "-T",
-        "-s",
-        "-i",
-        str(arpa_path),
-        str(binary_lm_path),
+        "python", 
+        "/opt/rhasspy-asr-deepspeech-hermes/tools/generate_lm.py",
+        "--input_txt",
+        str(vocab_path),
+        "--output_dir", 
+        str(lm_binary_dir),
+        "--top_k", 
+        "500000", 
+        "--kenlm_bins", 
+        "/opt/rhasspy-asr-deepspeech-hermes/tools/kenlm", 
+        "--arpa_order", 
+        "5", 
+        "--max_arpa_memory", 
+        "50%",
+        "--arpa_prune", 
+        "0|0|1", 
+        "--binary_a_bits", 
+        "255", 
+        "--binary_q_bits", 
+        "8", 
+        "--binary_type", 
+        "trie", 
+        "--discount_fallback"
     ]
     _LOGGER.debug(binary_command)
     subprocess.check_call(binary_command)
 
-
-def make_trie(
+def make_scorer(
+    vocab_path: typing.Union[str, Path],
+    scorer_file: typing.Union[str, Path],
     alphabet_path: typing.Union[str, Path],
-    binary_lm_path: typing.Union[str, Path],
-    trie_path: typing.Union[str, Path],
+    tmp_folder: typing.Union[str, Path]
 ):
     """Generate trie using Mozilla native-client tool."""
+
+    _LOGGER.debug("Create Scorerfile")
+    _LOGGER.debug("With: vocab_path: %s | scorer_file: %s | alphabet_path: %s | lm_binary_dir: %s", vocab_path, scorer_file, alphabet_path, tmp_folder)
+     
     trie_command = [
-        "generate_trie",
-        str(alphabet_path),
-        str(binary_lm_path),
-        str(trie_path),
+        "python3", 
+        "/opt/rhasspy-asr-deepspeech-hermes/tools/generate_package.py", 
+        "--alphabet", 
+        str(alphabet_path), 
+        "--lm", 
+        str(tmp_folder) + "/lm.binary", 
+        "--vocab",
+        str(vocab_path), 
+        "--package", 
+        str(scorer_file), 
+        "--default_alpha", 
+        "0.931289039105002", 
+        "--default_beta", 
+        "1.1834137581510284"
     ]
 
     _LOGGER.debug(trie_command)
